b5a0c6862ca51ebed7df8234fa3b60e380291c30
Danilo Guimarães <d.guimaraes.dev@gmail.com>
2026-02-14 19:55:32 -0300

    🚀 Python Logic & Algorithms: De Iniciante a Desenvolvedor Repositório focado na resolução de desafios de lógica, tratamento de dados e automação básica. Um registro fiel da minha evolução técnica durante a graduação em ADS na Estácio e o Curso em Vídeo. 🐍💻

--- exercicios_curso_em_video/0019_analisador_de_nomes.py
+++ exercicios_curso_em_video/0019_analisador_de_nomes.py
@@ -1,8 +1,6 @@
-nome = input('Qual o seu nome completo?\n')
+nome = input('Qual o seu nome completo?\n').strip()
 print(nome.upper()) # Exibe o nome todo em maiúsculas
 print(nome.lower()) # Exibe o nome todo em minúsculas
 letra = len("".join(nome.split())) # Junta as palavras sem espaços e conta o total de letras
-print(letra)
-
-nomes = nome.split() [0] # Divide o nome e pega apenas a primeira palavra (índice 0)
-print(f"{nomes}")
+print('Seu nome tem {} letras'.format(letra))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
